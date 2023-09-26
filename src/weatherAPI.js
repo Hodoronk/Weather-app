95f70e2f46058cbb476ee7b8cf4c721313692c6c
Hodoronk <tanasa.andrei@hotmail.com>
2023-09-26 19:09:20 +0300

    implemented image change and API textcontent changes

--- src/weatherAPI.js
+++ src/weatherAPI.js
@@ -1,44 +1,99 @@
+const { format } = require('date-fns');
+
+
+
+const body = document.querySelector('body') ;
+
+// Search bar declarations
+const input = document.getElementById ('search-bar') ; 
+const searchBtn = document.getElementById ('search-btn' ) ;
+
+// Today Weather declarations
+const location = document.getElementById ('location') ;
+const tempNow = document.getElementById ('temperature') ;
+const feelsLike = document.getElementById ('fl-temp') ;
+const humidity = document.getElementById ('humid-percent') ;
+const chanceOfRain = document.getElementById('cor-percent') ;
+const windSpeed = document.getElementById('w-speed') ;
+const todayImage = document.getElementById('weather-icon') ;
+const todayDate = document.getElementById('today-date') ;
+
+
 
 export async function getWeatherData(location) {
 
-    const getApi = await fetch(`http://api.weatherapi.com/v1/current.json?key=dc888092a84f491faf694133232509&q=${location}&aqi=no` , {mode : "cors"}) ;
-    const response = await getApi.json();
+    const getWeather = await fetch(`http://api.weatherapi.com/v1/current.json?key=dc888092a84f491faf694133232509&q=${location}&aqi=no` , {mode : "cors"}) ;
+    const weatherResponse = await getWeather.json();
+    console.log(weatherResponse);
     
+    const getForecast = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=dc888092a84f491faf694133232509&q=${location}&aqi=no` , {mode: "cors"}) ;
+    const forecastResponse = await getForecast.json() ;
+    console.log(forecastResponse) ;
+
+
     // Location related 
-    const localTime = response.location.localtime;
-    const region = response.location.region;
+    const localTime = weatherResponse.location.localtime;
+    const region = weatherResponse.location.region;
 
     // Weather Related
-    const tempC = response.current.temp_c;
-    const feelsLikeC = response.current.feelslike_c;
-    const tempF = response.current.temp_f;
-    const feelsLikeF = response.current.feelslike_f;
-    const cloudStatus = response.current.cloud;
-    const humidity = response.current.humidity;
-    const isDay = response.current.is_day;
-    const uvStatus = response.current.uv;
-    const windDirection = response.current.wind_dir;
-    const windSpeedKm = response.current.wind_kph;
-    const windSpeedMph = response.current.wind_mph
+    const tempC = weatherResponse.current.temp_c;
+    const feelsLikeC = weatherResponse.current.feelslike_c;
+    const tempF = weatherResponse.current.temp_f;
+    const feelsLikeF = weatherResponse.current.feelslike_f;
+    const cloudStatus = weatherResponse.current.cloud;
+    const humidityApi = weatherResponse.current.humidity;
+    const isDay = weatherResponse.current.is_day;
+    const uvStatus = weatherResponse.current.uv;
+    const windDirection = weatherResponse.current.wind_dir;
+    const windSpeedKm = weatherResponse.current.wind_kph;
+    const windSpeedMph = weatherResponse.current.wind_mph
 
-    console.log(`
-    Location: ${location} ;
-    Region: ${region} ;
-    Local Time: ${localTime} ;
+    // Text content modifications
+    todayDate.textContent = getToday();
 
-    Temperature in Celsius: ${tempC} ;
-    Feels like: ${feelsLikeC} ;
 
-    Temperature in Fahrenheit: ${tempF} ;
-    Feels like: ${feelsLikeF} ;
 
-    How cloudy it is on a scale from 1 to 100 : ${cloudStatus} ;
-    Humidity: ${humidity} ;
-    Is it day? ${isDay} ;
-    UV status: ${uvStatus} ;
-    Wind Direction: ${windDirection} ;
-    Wind Speed in km/h: ${windSpeedKm} ;
-    Wind Speed in mp/h: ${windSpeedMph} ;
-    `)
+
+
+
+
+    tempNow.textContent = tempC + '°C';
+    feelsLike.textContent = feelsLikeC + '°C' ;
+    humidity.textContent = humidityApi + "%"; 
+    chanceOfRain.textContent = forecastResponse.forecast.forecastday[0].day.daily_chance_of_rain + '%' ;
+    windSpeed.textContent = windSpeedKm + 'km' ;
+
+    //Text content modifications for forecast
+
+    // Image modifications
+    if (forecastResponse.forecast.forecastday[0].day.daily_chance_of_rain >= 50){
+        todayImage.src = '/src/images/rain.png' ;
+    } else if(cloudStatus == 0) {
+        todayImage.src = '/src/images/clear.png' ;
+    } else if(forecastResponse.forecast.forecastday[0].day.daily_chance_of_rain >= 50) {
+        todayImage.src = '/src/images/snow.png' ;
+    } else if(cloudStatus >= 20) {
+        todayImage.src = '/src/images/clouds.png' ;
+    }
+
+
 }
 
+
+
+searchBtn.addEventListener('click' , () => {
+
+    getWeatherData(input.value)
+    location.textContent = input.value;
+
+
+})
+
+
+const getToday = () => {
+    const today = new Date();
+    const formattedDate = format(today , 'EEEE')
+
+
+   return formattedDate;
+}
