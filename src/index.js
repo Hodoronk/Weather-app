fa0f6262ce1ebadce76408aaf4b60e26078f8090
Hodoronk <tanasa.andrei@hotmail.com>
2023-09-27 13:43:56 +0300

    added onLoad function that loads accurate info at first load

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-import { getWeatherData } from "./weatherAPI";
+import { getWeatherData, getToday, todayDate, location} from "./weatherAPI";
+const { format } = require('date-fns');
 
 
+const onLoad = () => {
+
+    const today = new Date();
+    const formattedDate = format(today , 'EEEE, d MMMM yyyy') ;
+    todayDate.textContent = formattedDate;
+    location.textContent = 'Lupeni' ;
+    getWeatherData('Lupeni') ;
+
+}
 
 
+onLoad();
