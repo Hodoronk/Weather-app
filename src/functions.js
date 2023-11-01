abbaa58cd1df83c76b7dc826929f01c58ab534e1
Hodoronk <tanasa.andrei@hotmail.com>
2023-11-01 16:29:20 +0200

    added function for determining weather icons

--- src/functions.js
+++ src/functions.js
@@ -19,3 +19,22 @@ export const kToFahr = (temp) => {
     let result = 9/5 * (temp - 273.15) + 32
     return result = result.toFixed(1)
 }
+
+
+export function imageChange(code, image) {
+    if(code > 199 && code < 233) {
+        image.src = "https://openweathermap.org/img/wn/11d@2x.png"
+    } else if(code > 299 && code < 321) {
+        image.src = "https://openweathermap.org/img/wn/09d@2x.png"
+    } else if(code > 499 && code < 532) {
+        image.src = "https://openweathermap.org/img/wn/10d@2x.png"
+    } else if(code > 599 && code < 623) {
+        image.src = "https://openweathermap.org/img/wn/13d@2x.png"
+    } else if(code > 700 && code < 782) {
+        image.src = "https://openweathermap.org/img/wn/01d@2x.png"
+    } else if(code === 800) {
+        image.src = "https://openweathermap.org/img/wn/10d@2x.png"
+    } else if(code > 800 && code < 805) {
+        image.src = "https://openweathermap.org/img/wn/02d@2x.png"
+    }
+}
