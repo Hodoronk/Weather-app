3ff483959793055a15d8093cce28488b66dfa34e
Hodoronk <tanasa.andrei@hotmail.com>
2023-11-02 17:26:51 +0200

    up to date

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
 const path = require("path");
 
 module.exports = {
+  mode: 'production',
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  optimization: {
+    usedExports: true,
+    minimize: true,
+  },
+
   module: {
     rules: [
       {
